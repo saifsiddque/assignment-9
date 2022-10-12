47497c4bc8dbf7a93977b30a063d7635fc99b5b8
saif <abutalha666124@gmail.com>
2022-10-12 17:20:36 +0600

    Alhamdulillah

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
   return (
     <div className=''>
       <h1 className='text-4xl m-5 border-b-2'>Quiz MASTER</h1>
-      <div className='grid gap-6 row-gap-5 mb-8 lg:grid-cols-4 sm:row-gap-6 sm:grid-cols-2 px-10'>
+      <div className='grid gap-6 row-gap-5 mb-8 lg:grid-cols-4 sm:row-gap-6 sm:grid-cols-2 px-10 '>
           {
             quizData.map(quiz=>(
               <Quizs key={quiz.id} quiz={quiz}></Quizs>
