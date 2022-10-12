6fb0939da8eb20b659820289c5a2423944dcc321
saif <abutalha666124@gmail.com>
2022-10-12 23:40:32 +0600

    finish

--- react-app/src/components/Quizquestion.js
+++ react-app/src/components/Quizquestion.js
@@ -8,12 +8,11 @@ const Quizquestion = ({qui}) => {
     console.log(ans) 
 
         if (ans === qui.correctAnswer) {
-            return toast( "Wow so easy!" );
+            return toast( "You Are Right" );
           }
-          return toast("Wow so hard");
+          return toast("You Are Wrong");
         
     } 
-    console.log(qui.correctAnswer);
     const [isActive, setIsActive] = useState(false);
     const handleClick = () => {
       setIsActive(current => !current);
@@ -34,7 +33,7 @@ const Quizquestion = ({qui}) => {
                 {
                     qui.options.map(option=>(
                         <div option={option} className="mt-5 flex items-center pl-4 rounded border border-gray-200 dark:border-gray-700">
-                            <input onClick={() => notify(qui.options[0], 0)}  type="radio"  name="bordered-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                            <input onClick={() => notify({option})}  type="radio"  name="bordered-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                             <label   className="py-4 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">{option}</label>
                         </div>
                     ))
