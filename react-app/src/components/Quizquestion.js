c5b371ce92c9a06f6e95699a647be8bb198347f7
saif <abutalha666124@gmail.com>
2022-10-18 16:29:26 +0600

    Alhamdulillah Finish

--- react-app/src/components/Quizquestion.js
+++ react-app/src/components/Quizquestion.js
@@ -6,11 +6,15 @@ import 'react-toastify/dist/ReactToastify.css';
 const Quizquestion = ({qui}) => {
     const notify = (ans) =>{
     console.log(ans) 
+    console.log(qui)
 
-        if (ans === qui.correctAnswer) {
-            return toast( "You Are Right" );
+        if (ans.option === qui.correctAnswer) {
+             toast( "You Are Right" );
           }
-          return toast("You Are Wrong");
+          else{
+            toast("You Are Wrong");
+
+          }
         
     } 
     const [isActive, setIsActive] = useState(false);
@@ -32,7 +36,7 @@ const Quizquestion = ({qui}) => {
 
                 {
                     qui.options.map(option=>(
-                        <div option={option} className="mt-5 flex items-center pl-4 rounded border border-gray-200 dark:border-gray-700">
+                        <div option={option.option} className="mt-5 flex items-center pl-4 rounded border border-gray-200 dark:border-gray-700">
                             <input onClick={() => notify({option})}  type="radio"  name="bordered-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                             <label   className="py-4 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">{option}</label>
                         </div>
